92b4f5c6a1b983814cb7950309c3185baa611a84
abenidev <abenidev@gmail.com>
2023-03-09 20:31:42 +0300

    add background play

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:zema/constants/colors.dart';
 import 'package:zema/constants/common.dart';
 import 'package:zema/constants/globals.dart';
@@ -11,10 +12,16 @@ import 'package:zema/screens/home_screen.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
+
   runApp(
     MultiProvider(
       providers: [
